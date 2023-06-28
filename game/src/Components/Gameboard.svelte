2acdf8c4fe537ec62f66c9f0ac53c83a44c22aa5
iraklidzaganishvili <dzaganishviliirakli@gmail.com>
2023-06-28 21:05:04 +0400

    Update Gameboard.svelte

--- game/src/Components/Gameboard.svelte
+++ game/src/Components/Gameboard.svelte
@@ -20,6 +20,8 @@
         //     width: gameboard.offsetWidth,
         //     height: gameboard.offsetWidth / 2
         // });
+
+        //Draw canvas
         let canvas = document.getElementById('gameboard');
         let ctx = canvas.getContext('2d');
         size = canvas.getBoundingClientRect();
@@ -27,7 +29,24 @@
         canvas.height = size.height * 10;
         ctx.scale(10, 10);
         ctx.beginPath();
-        ctx.rect(20, 20, size.width-300, size.height-300); 
+        ctx.rect(20, 20, 300, 300); 
         ctx.fill();
+        
+        //Styling stuff
+        window.addEventListener('resize', resize);
+        function resize(){
+            let viewportWidth = window.innerWidth;
+            let viewportHeight = window.innerHeight;
+            if (canvas.offsetWidth > viewportWidth || canvas.offsetHeight > viewportHeight) {
+                canvas.style.left = "0%";
+                canvas.style.top = "0%";
+                canvas.style.transform = "translate(0%, 0%)";
+            } else {
+                canvas.style.left = "50%";
+                canvas.style.top = "50%";
+                canvas.style.transform = "translate(-50%, -50%)";
+            }
+        }
+        resize();
     });
 </script>
